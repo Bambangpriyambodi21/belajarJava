8f078f6ba133bc6c1be66836393260e8dba15f76
Bambang Priyambodo <bambangpriyambodi21@gmail.com>
2024-05-01 10:21:36 +0700

    [FEATURE] add new logic test PT DIKA

--- src/Main.java
+++ src/Main.java
@@ -142,7 +142,20 @@ public class Main {
             e.printStackTrace();
         }
     }
+    static void dika1(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input : ");
+        int input = scanner.nextInt();
+        System.out.println("Output :");
+        for(int i=1;i<=input;i++){
+            for (int j=1;j<=i*2;j++){
+                if (j%2!=0){
+                    System.out.print(j+" ");
+                }
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
-
     }
 }
